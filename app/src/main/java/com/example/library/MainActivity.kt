aad601efb7f5d4eb1431f5e92d2720b11cd234be
Eduardo <eduardomejiac@hotmail.com>
2019-03-20 20:40:55 +0100

    Show Detail class created also created some icons

--- app/src/main/java/com/example/library/MainActivity.kt
+++ app/src/main/java/com/example/library/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.library
-
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+        val showDetailBook = ShowDetailBook()
+
+        supportFragmentManager.beginTransaction().replace(R.id.maincontainer,showDetailBook).commit()
+
         //First line to commit
     }
+
 }
