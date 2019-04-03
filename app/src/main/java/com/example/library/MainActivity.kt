ff1eb92c322d41ca2fde60b066bba6965bb7bc29
Dragomir Galinov <d.galinov.d.salle@gmail.com>
2019-04-03 20:44:07 +0200

    View Login & Register

--- app/src/main/java/com/example/library/MainActivity.kt
+++ app/src/main/java/com/example/library/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val ListView = ListBooks()
+        val ListView = LoginFragment()
 
         supportFragmentManager.beginTransaction().replace(R.id.maincontainer,ListView).commit()
 
