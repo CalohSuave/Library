c5f4b8678dd36885d40f6b4fcc711d79d4cfe0d5
Eduardo <emejiacabello@hotmail.com>
2019-04-06 00:48:59 +0200

    Auto-Text-Size para visualizar correctamente los items

--- app/src/main/java/com/example/library/MainActivity.kt
+++ app/src/main/java/com/example/library/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity(), ListBooks.OnButtonPressedListener{
     override fun onButton(books: Libro) {
 
         val detailFragment = ShowDetailBook.newInstance(books)
-        supportFragmentManager.beginTransaction().replace(R.id.maincontainer,detailFragment).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.maincontainer,detailFragment).addToBackStack(null).commit()
 
 
     }
@@ -19,13 +19,7 @@ class MainActivity : AppCompatActivity(), ListBooks.OnButtonPressedListener{
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-<<<<<<< HEAD
-
-        val ListView = LoginFragment()
-
-=======
         val ListView = ListBooks()
->>>>>>> 82af9f2ebba3dfefd4e1a6c73f9ad81d8f05b443
         supportFragmentManager.beginTransaction().replace(R.id.maincontainer,ListView).commit()
 
 
