c4758414833c86b759673263368ecb970589b052
Eduardo <emejiacabello@hotmail.com>
2019-04-09 02:49:00 +0200

    DRAGO  me has abandonado cual perro, pero lo he sacado adelante

--- app/src/main/java/com/example/library/MainActivity.kt
+++ app/src/main/java/com/example/library/MainActivity.kt
@@ -1,22 +1,22 @@
 package com.example.library
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.appcompat.widget.Toolbar
 import com.example.library.ListBooksFragment.OnButtonPressedListener
 import com.example.library.LoginFragment.onLogindPressed
 import com.example.library.RegisterFragment.onRegisterFragment
-import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity(), OnButtonPressedListener, onLogindPressed, onRegisterFragment{
+
+
     override fun onRegisterButtonPressed(email: String, name: String, Password: String) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-
     override fun onLoginFragmentRegister() {
-
-        val listBooks = ListBooksFragment()
-        supportFragmentManager.beginTransaction().replace(R.id.maincontainer, listBooks).addToBackStack(null).commit()
-
 
         val registerFragment = RegisterFragment()
         supportFragmentManager.beginTransaction().replace(R.id.maincontainer, registerFragment).addToBackStack(null)
@@ -50,16 +50,19 @@ class MainActivity : AppCompatActivity(), OnButtonPressedListener, onLogindPress
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setSupportActionBar(toolbar as Toolbar)
         val LoginFrag = LoginFragment()
         supportFragmentManager.beginTransaction().replace(R.id.maincontainer,LoginFrag).commit()
 
 
+    }
 
-
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
 
